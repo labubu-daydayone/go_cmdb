37628ec1cf6af0bd3446b60bfd92ebef7015317d
labubu-daydayone <237174967+labubu-daydayone@users.noreply.github.com>
2026-01-28 02:07:36 -0500

    Revert ID type changes

--- internal/model/base.go
+++ internal/model/base.go
@@ -6,7 +6,7 @@ import (
 
 // BaseModel contains common fields for all models
 type BaseModel struct {
-	ID        int64     `gorm:"primaryKey;autoIncrement" json:"id"`
+	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
